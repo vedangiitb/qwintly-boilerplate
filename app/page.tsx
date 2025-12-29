5e7f39a34159ea5744a96fead7d022a1ffb5b227
vedangiitb <vedangbaleiitb@gmail.com>
2025-12-29 10:04:15 +0530

    added shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 // The page component is used to render the home page.
 //  DESC_END
 
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-white dark:bg-black">
