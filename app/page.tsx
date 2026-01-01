419bd70eb6af498a4b836f2cb9d9d434364390f0
vedangiitb <vedangbaleiitb@gmail.com>
2026-01-01 09:58:12 +0530

    ts-validator testing

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,12 @@
 // The page component is used to render the home page.
 //  DESC_END
 
-import { Button } from "@/components/ui/button";
-
 export default function Home() {
+  const boilerplate : {name:string} = {name:'Boilerplate'}
   return (
     <div className="min-h-screen flex items-center justify-center bg-white dark:bg-black">
       <main className="p-8 text-center">
-        <h1 className="text-3xl font-semibold">Boilerplate</h1>
+        <h1 className="text-3xl font-semibold">{boilerplate.namee}</h1>
         <p className="mt-2 text-zinc-600 dark:text-zinc-400">
           Edit <code>app/page.tsx</code> to get started.
         </p>
