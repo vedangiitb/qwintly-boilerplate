33633d925c8ac0d4dc80db12ad224b0a2dbfde3d
vedangiitb <vedangbaleiitb@gmail.com>
2026-01-01 11:12:24 +0530

    ts-validator testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,10 @@
 //  DESC_END
 
 export default function Home() {
-  const boilerplate : {name:string} = {name:'Boilerplate'}
   return (
     <div className="min-h-screen flex items-center justify-center bg-white dark:bg-black">
       <main className="p-8 text-center">
-        <h1 className="text-3xl font-semibold">{boilerplate.namee}</h1>
+        <h1 className="text-3xl font-semibold">Boilerplate</h1>
         <p className="mt-2 text-zinc-600 dark:text-zinc-400">
           Edit <code>app/page.tsx</code> to get started.
         </p>
