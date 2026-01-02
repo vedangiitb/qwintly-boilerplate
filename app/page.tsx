248b913464c1ccee7875b69c58f2462ee6eb6a97
vedangiitb <vedangbaleiitb@gmail.com>
2026-01-02 11:35:51 +0530

    deployer validation testing

--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,11 @@
 //  DESC_END
 
 export default function Home() {
+  const name: { name: string } = { name: "Qwintly" };
   return (
     <div className="min-h-screen flex items-center justify-center bg-white dark:bg-black">
       <main className="p-8 text-center">
-        <h1 className="text-3xl font-semibold">Boilerplate</h1>
+        <h1 className="text-3xl font-semibold">Boilerplate {name.name}</h1>
         <p className="mt-2 text-zinc-600 dark:text-zinc-400">
           Edit <code>app/page.tsx</code> to get started.
         </p>
