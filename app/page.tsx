375532544271ecfcbe3fa815e0550834611bce02
vedangiitb <vedangbaleiitb@gmail.com>
2026-05-12 17:49:22 +0530

    page config updates

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
-import { config } from "./page.config";
 import { RenderElement } from "@/lib/renderer/RenderElement";
+import pageConfig from "./pageConfig.json";
+import type { BuilderElement } from "@/types/elements";
 
 export default function Page() {
+  const config = pageConfig as { elements: BuilderElement[] };
   return config.elements.map((el) => <RenderElement key={el.id} el={el} />);
 }
